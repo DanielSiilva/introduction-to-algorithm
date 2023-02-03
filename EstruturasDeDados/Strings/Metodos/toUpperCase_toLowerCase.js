cdabe47beeffbdfb1c36991363c4b96250a93d6b
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2023-02-03 11:44:24 -0300

    Feat: string - metodos: toUpperCase toLowerCase

---
+++ EstruturasDeDados/Strings/Metodos/toUpperCase_toLowerCase.js
@@ -0,0 +1,14 @@
+//toUpperCase() : este método muda a string para letras maiúsculas.
+console.log('Metodo para letras Maisculas');
+const NovoMetodo = 'daniel sousa'
+
+console.log(`${NovoMetodo}, ${NovoMetodo.toLocaleUpperCase()}`)
+
+console.log('\r\n');
+
+//toLowerCase() : este método muda a string para letras minúsculas
+console.log('Metodo para letras Minusculas');
+
+const upperCase = 'DANIEL SOUSA'
+console.log(`${upperCase}, ${upperCase.toLocaleLowerCase()}`)
+
