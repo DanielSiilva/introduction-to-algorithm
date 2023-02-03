90ac448fc08a84fbc8e3e1eec100477a06a00683
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2023-02-03 10:58:27 -0300

    Feat: String - Definicão

--- EstruturasDeDados/Strings/Metodos/metodos_string.js
+++ EstruturasDeDados/Strings/Metodos/metodos_string.js
@@ -1,1 +1,12 @@
-console.log("Metodos");
+//Tudo em Js, é um objeto. Nesse caso, string são objetos, e são objetos imutaveis. Ou seja, não podemos alterar depois de criada, sendo um dado primitivos. 
+//Porém, exitem diversos metodos que podemos manipular strings.
+
+//Ex> Length server para sabermos o comprimento/tamanho  de uma string.
+
+let Name = 'Daniel'
+
+console.log(Name.length); // 6
+
+
+
+ //Tudo em JavaScript é um objeto. Uma string é um tipo de dado primitivo, o que significa que não podemos modificá-la depois de criada. O objeto string tem muitos métodos string. Existem diferentes métodos de string que podem nos ajudar a trabalhar com strings.
