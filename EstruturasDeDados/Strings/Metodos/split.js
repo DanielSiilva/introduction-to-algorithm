b9cc89a3feef35f19a1a19f5bdd0632027566984
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2023-02-03 12:35:07 -0300

    feat: string - metodos: substting e split

---
+++ EstruturasDeDados/Strings/Metodos/split.js
@@ -0,0 +1,10 @@
+// O método split() divide uma String em uma lista ordenada de substrings, coloca essas substrings em um array e retorna o array. A divisão é feita procurando um padrão, onde o padrão é fornecido como o primeiro parâmetro na chamada do método.
+
+
+let newNames = 'Daniel MArlene Andreane Ivaldo'
+
+//Com espaço separa apenas as palavras
+console.log(newNames.split(' '));
+
+//Sem espaço, separa todos as letras
+
